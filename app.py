2fe7631b92750bd39c7d10ba6d67cd7e203809c9
babaraza <syedrazatx@gmail.com>
2019-08-02 16:57:19 -0500

    Got Weather API working

--- app.py
+++ app.py
@@ -1,26 +1,45 @@
 import requests
-from bs4 import BeautifulSoup
-
-# TODO:
-#   Get historical temperature reading from api or website
-#   Open Existing Excel file for appending data
-#   Append Temperature readings in correct columns
-
-
-url = 'https://www.wunderground.com/history/monthly/KSGR/' \
-      'date/2017-8?req_city=Richmond&req_state=TX&req_statename=Texas&reqdb' \
-      '.zip=77469&reqdb.magic=1&reqdb.wmo=99999'
+import calendar
 
 s = requests.Session()
-resp = s.get(url)
-resp.raise_for_status()
-soup = BeautifulSoup(resp.text, "html.parser")
 
-test = soup.findAll('td')
 
-print(soup)
-# for header in test:
-#     if header.text == "Avg Temperature":
-#         print("found it")
-#     else:
-#         print("not working")
+def get_temps():
+    # Using manual array since we need leading 0 in months for url
+    months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+
+    # Ask for start and end year, if same year is entered it will get 1 year of temps
+    start_year = int(input("Enter Start year > "))
+    end_year = int(input("Enter End year > "))
+
+    # Make the range inclusive
+    end_year += 1
+
+    # Iterate over years and months
+    for year in range(start_year, end_year):
+        for month in months:
+
+            # Get the last day of the month for url using calendar
+            last_day = calendar.monthrange(year, int(month))[1]
+
+            # Date link inside url example: 2019010120190131 (start and end date)
+            date_link = '{0}{1}01{0}{1}{2}'.format(year, month, last_day)
+            final_url = 'https://api-ak.wunderground.com/api/75e91f5c866f39a2/history_' \
+                        + date_link + \
+                        '/lang:EN/units:english/bestfct:1/v:2.0/q/KHOU.json?showObs=0&ttl=120'
+            get_data(final_url)
+
+
+def get_data(url):
+    r = s.get(url)          # Open final url created
+    r.raise_for_status()    # Check for errors
+    resp = r.json()         # Get JSON response
+
+    try:
+        # Get Average Maximum Temperature for the month
+        print(resp['history']['summary']['max_temperature_avg'])
+    except KeyError:        # If data is unavailable print NA
+        print("N/A")
+
+
+get_temps()
