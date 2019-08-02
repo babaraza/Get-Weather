83c04d7086ae40d66ea717d1486aaf77133753f7
sb-raza <syedrazatx@gmail.com>
2019-08-01 22:42:04 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import requests
+
+# TODO:
+#   Get historical temperature reading from api or website
+#   Open Existing Excel file for appending data
+#   Append Temperature readings in correct columns
+
